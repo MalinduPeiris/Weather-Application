c07c8ccb68208ad08f90104392cff307a8b5a3bf
Malindu Peiris <malindupeiris5@gmail.com>
2023-10-02 18:14:47 +0530

    update code

--- js/home.js
+++ js/home.js
@@ -1,43 +1,54 @@
-const navsearch=document.getElementById("navsearch");
-
-navsearch
+getLocation();
 
 
+// function search(){
+//     let location=document.getElementById("inputSearchTxt").value;
+//     alert(location);
+//     serchLocation(location);
+// }
+
+document.getElementById("btn-search").addEventListener("click",(e)=>{
+    e.preventDefault();
+    let location=$("#inputSearchTxt").val();
+    // alert(location);
+    console.log(location);
+    serchLocation(location);
+})
+let icon=$("#icon");
+let temp=$("#tempText");
+let locationName=$("#locationName");
+let country =$("#country");
+let city = $("#city");
+let humidity =$("#humiditytxt");
+
+function serchLocation(location){
+  
 
 
+    $.ajax({
+        method : "GET",
+        url :`http://api.weatherapi.com/v1/current.json?key=123d678a6bd74c81b3f165334230110&q=${location}`,
+        success :(data)=>{
+            console.log(data);
+            icon.attr("src",data.current.condition.icon);
+            temp.text(data.current.temp_c+"°C");
+            locationName.text(data.location.tz_id);
+            country.text(data.location.country);
+            city.text(data.location.name);
 
+        }
+    })
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    // x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+    let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    serchLocation(lat+","+lon);
+}
